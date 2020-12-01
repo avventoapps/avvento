f674a7ec0ce394e34fff2da0c9e990bbc4259cd1
k-joseph <kaweesi@jembi.org>
2020-12-01 22:17:10 +0300

    improved some design dimensions

--- AvventoRadio/app/src/main/java/org/avvento/apps/onlineradio/MainActivity.java
+++ AvventoRadio/app/src/main/java/org/avvento/apps/onlineradio/MainActivity.java
@@ -9,10 +9,10 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -35,19 +35,22 @@ public class MainActivity extends AppCompatActivity implements Serializable {
     private NotificationManager notificationManager;
     private Intent buttonIntent;
 
-    public boolean getPlaying() {
-        return playing;
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         instance = this;
         setContentView(R.layout.activity_main);
+
+        //allow running all on the main thread
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+        ((TextView) findViewById(R.id.warning)).setText(getStatus());
+
         streamBtn = findViewById(R.id.audioStreamBtn);
         notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
-        ((TextView) findViewById(R.id.status)).setText(getStatus());
         initialise();
     }
 
